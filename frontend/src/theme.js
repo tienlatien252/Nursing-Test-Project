f58ee0f03c26681534b168e9d2fa5bdeb5688c43
linhvovo <linhtmv@gmail.com>
2020-07-29 13:04:32 -0500

    clean up code

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -4,12 +4,6 @@ const useStyles = makeStyles((theme) => ({
     root: {
       flexGrow: 1,
     },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
 }));
 
 export default useStyles;
