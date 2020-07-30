365ef6a5d1bde277ed511f20c2e5239d926eecc7
linhvovo <linhtmv@gmail.com>
2020-07-30 17:02:00 -0500

    set up basic header

--- frontend/src/views/ProfilePage/ProfilePage.js
+++ frontend/src/views/ProfilePage/ProfilePage.js
@@ -47,7 +47,7 @@ export default function ProfilePage(props) {
     <div>
       <Header
         color="transparent"
-        brand="Material Kit React"
+        brand="Nursing Test"
         rightLinks={<HeaderLinks />}
         fixed
         changeColorOnScroll={{
