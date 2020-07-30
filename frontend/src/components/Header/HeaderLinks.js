6d98ec221cfb6e48c3b237c6393cf4a9c447ce8f
linhvovo <linhtmv@gmail.com>
2020-07-30 17:37:23 -0500

    clean up code

--- frontend/src/components/Header/HeaderLinks.js
+++ frontend/src/components/Header/HeaderLinks.js
@@ -1,9 +1,5 @@
 /*eslint-disable*/
 import React from "react";
-import DeleteIcon from "@material-ui/icons/Delete";
-import IconButton from "@material-ui/core/IconButton";
-// react components for routing our app without refresh
-import { Link } from "react-router-dom";
 
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
