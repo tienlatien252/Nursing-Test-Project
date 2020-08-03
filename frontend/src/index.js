28e5c0f136e473bcac60a208ac827f36919fc41c
linhvovo <linhtmv@gmail.com>
2020-08-03 14:31:50 -0500

    configure Auth0Provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 import "assets/scss/material-kit-react.scss?v=1.9.0";
 
@@ -14,13 +15,19 @@ import SignupPage from "views/SignupPage/SignupPage.js";
 var hist = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      <Route path="/" component={LandingPage} exact />
-      <Route path="/profile-page" component={ProfilePage} />
-      <Route path="/login-page" component={LoginPage} />
-      <Route path="/signup-page" component={SignupPage} />
-    </Switch>
-  </Router>,
+  <Auth0Provider
+    domain="dev-e14kaf0s.us.auth0.com"
+    clientId="BISSFueKaVtZnQ5dZOOYHXPlYB23O9w3"
+    redirectUri={window.location.origin}
+  >
+    <Router history={hist}>
+      <Switch>
+        <Route path="/" component={LandingPage} exact />
+        <Route path="/profile-page" component={ProfilePage} />
+        <Route path="/login-page" component={LoginPage} />
+        <Route path="/signup-page" component={SignupPage} />
+      </Switch>
+    </Router>
+  </Auth0Provider>,
   document.getElementById("root")
 );
