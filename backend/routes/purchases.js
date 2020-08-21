3f80b540dac844cc3770f136ab4fb7556d1329e1
An Vo <thaiankh@gmail.com>
2020-08-20 21:30:46 -0500

    add testing purchase endpoint and fix the environment variable bug

--- backend/routes/purchases.js
+++ backend/routes/purchases.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const getPurchases = require('../controllers/purchases-controller')
+const client = require('../postresql_client');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', getPurchases)
-
-
+router.get('/', async function getPurchases(req, res, next) {
+    const user_id = 'BN5rM3YwOOe7qk0bdqul2XRJZ4I3';
+    const purchases = await client.query(`SELECT * FROM purchases WHERE user_id='${user_id}'`);
+    const response = {
+        'uid': user_id,
+        'purchase': purchases.rows
+    };
+    return res.json(response);
+})
 
 module.exports = router;
