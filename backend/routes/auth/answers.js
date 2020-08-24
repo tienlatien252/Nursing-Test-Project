b9c305a0314be3f0bf7207f9cc193ec2850cea2c
An Vo <thaiankh@gmail.com>
2020-08-23 22:37:59 -0500

    Change some variable names

--- backend/routes/auth/answers.js
+++ backend/routes/auth/answers.js
@@ -1,13 +1,13 @@
 const client = require('../../postresql_client');
 
-async function getCorrectAnswers(submittion, testId){
-    const userQuestion = submittion.map((answer)=>answer.questionId).toString();
+async function getCorrectAnswers(submission, testId){
+    const userQuestion = submission.map((answer)=>answer.questionId).toString();
     const queryString = `SELECT * FROM questions WHERE test_id='${testId}' AND question_id IN (${userQuestion});`;
     const questions = (await client.query(queryString)).rows;
 
-    const correctAnswers = questions.reduce((accumulator, question) =>{
-        accumulator[question.question_id]=question.correct_answer;
-        return accumulator;
+    const correctAnswers = questions.reduce((questionAnswerMap, question) =>{
+        questionAnswerMap[question.question_id]=question.correct_answer;
+        return questionAnswerMap;
     }, {})
 
     return correctAnswers;
@@ -15,15 +15,14 @@ async function getCorrectAnswers(submittion, testId){
 
 async function postAnswers(req, res, next) {
     try {
-        const testId = req.params.testId;
-        const userSubmition = req.body.answers;
-        const correctAnswers = await getCorrectAnswers(userSubmition, testId);
+        const userSubmission = req.body.answers;
+        const correctAnswers = await getCorrectAnswers(userSubmission, req.params.testId);
 
-        const score = userSubmition.reduce((calculatingScore, answer) => {
-            return answer.answer == correctAnswers[answer.questionId] ? calculatingScore+1 : calculatingScore;
-        }, 0)*100.0/userSubmition.length;
+        const score = userSubmission.reduce((calculatingScore, userAnswer) => {
+            return userAnswer.answer == correctAnswers[userAnswer.questionId] ? calculatingScore+1 : calculatingScore;
+        }, 0)*100.0/userSubmission.length;
 
-        const answers = userSubmition.map((answer) => {
+        const answers = userSubmission.map((answer) => {
             return {
                 'questionId': answer.questionId,
                 'userAnswer': answer.answer,
