a3cd0ed0b2a248aa22449b5c3c4b0d021f443221
An Vo <thaiankh@gmail.com>
2020-08-26 21:03:43 -0500

    fix populate database query

--- backend/scripts/populate_db.py
+++ backend/scripts/populate_db.py
@@ -1,22 +1,23 @@
+#!python3
 import psycopg2
 import csv
 import os
 
-file = r"/Users/conghoangtungnguyen/Desktop/congtung.csv"
+file = r"congtung_2.csv"
 #connect to db
 conn = psycopg2.connect(
-    database=
-    user=
-    host=
-    password=
+    database="",
+    user="",
+    host="",
+    password="",
 )
+
 cur = conn.cursor()
 with open (file,'r') as f:
     reader = csv.reader(f)
     next(reader)
     for row in reader:
-           cur.execute(
-               "INSERT INTO questions (question_id, description, topic, picture_link, correct_answer) VALUES {}".format("(10,'which of the following','abc','abc','which has')"),
-            row
-           )
+        query = cur.mogrify("INSERT INTO questions (test_id, description, topic, picture_link, correct_answer, answers) VALUES (%s, %s, %s, %s, %s, %s)", (row[0], row[2], row[3], row[4], row[5], row[6]))
+        cur.execute(query)
+
 conn.commit()
