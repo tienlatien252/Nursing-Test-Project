a92b5612838e219761853cb5989bf042b7d33878
Cong Nguyen <howardnguyen69@gmail.com>
2020-08-29 17:29:55 -0500

    random selection

--- backend/routes/auth/questions.js
+++ backend/routes/auth/questions.js
@@ -1,13 +1,12 @@
 const client = require('../../postresql_client');
+
 /*Check the user purchases */
 async function getQuestions(req, res, next) {
-    //console.log(req.query)
     try {
         const testId = req.params.testId;
         const limit = req.query.limit || 20;
-        const queryString = `SELECT test_id, description, topic, picture_link, correct_answer, answers FROM questions WHERE test_id = ${testId} ${isNaN(limit)?'':'LIMIT ' + limit}`; 
+        const queryString = `SELECT test_id, question_id, description, topic, picture_link, correct_answer, answers FROM questions WHERE test_id = ${testId} ORDER BY random() ${isNaN(limit) ? '' : 'LIMIT ' + limit}`; 
         const questions = await client.query(queryString);
-        //console.log(questions)
         const response = {
             'questions': questions['rows']
         };
@@ -15,10 +14,7 @@ async function getQuestions(req, res, next) {
     } catch (error) {
         console.log(`Error in function getQuestions: ${error.message}`);
         return res.status(500).send({ error: 'Something went wrong!'});
-
     }
-
 }
-
 
 module.exports = getQuestions;
