f920b20943a66291875d854fbb1d9da3748f46f8
Cong Nguyen <howardnguyen69@gmail.com>
2020-09-07 17:31:43 -0500

    stripe_webhook

--- backend/routes/stripe_webhook.js
+++ backend/routes/stripe_webhook.js
@@ -2,12 +2,16 @@ const stripe = require('stripe')('pk_test_51HHZhMHCXVS0Bcw5gzo4yE1Lhyj6zkvdjcNDL
 const bodyParser = require('body-parser');
 const express = require('express');
 const router = express.Router();
+const endpointSecret = 'whsec_...';
 
-router.post('/', bodyParser.raw({ type: 'application/json' }), function (request, response) {
+router.post('/', bodyParser.raw({ type: 'application/json' }), async function (request, response) {
+    console.log(123);
+    const sig = request.headers['stripe-signature'];
     let event;
     try {
-        event = JSON.parse(request.body);
-    } catch (err) {
+        event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    }
+    catch (err) {
         response.status(400).send(`Webhook Error: ${err.message}`)
     }
 
