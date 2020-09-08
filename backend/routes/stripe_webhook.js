39f50cd687e3b97b7de0f0d5c52ab631829bd627
Cong Nguyen <howardnguyen69@gmail.com>
2020-09-08 17:14:52 -0500

    moment package postresql

--- backend/routes/stripe_webhook.js
+++ backend/routes/stripe_webhook.js
@@ -2,23 +2,27 @@ const stripe = require('stripe')('pk_test_51HHZhMHCXVS0Bcw5gzo4yE1Lhyj6zkvdjcNDL
 const bodyParser = require('body-parser');
 const express = require('express');
 const router = express.Router();
-const endpointSecret = 'whsec_...';
+const endpointSecret = 'whsec_DFvXlyhOHqHyu79fEi69H52CtT6gCTrV';
+const client = require('../../postresql_client');
 
 router.post('/', bodyParser.raw({ type: 'application/json' }), async function (request, response) {
-    console.log(123);
     const sig = request.headers['stripe-signature'];
     let event;
+    console.log(request.body);
     try {
         event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+        console.log("test event: ",event);
     }
     catch (err) {
+        console.log({ message: err.message, Error: err });
         response.status(400).send(`Webhook Error: ${err.message}`)
     }
-
     // Handle the event
     switch (event.type) {
         case 'payment_intent.succeeded':
             const paymentIntent = event.data.object;
+            const queryString = `INSERT INTO purchases(purchase_time, expire_time) VALUES (TO_TIMESTAMP(${event.data.object.created},'YYYY-MM-DD HH:MI:SS'),TO_TIMESTAMP(${event.data.object.created} + 378432000,'YYYY-MM-DD HH:MI:SS') )`;
+            
             console.log('PaymentIntent was successful!');
             break;
         case 'payment_method.attached':
