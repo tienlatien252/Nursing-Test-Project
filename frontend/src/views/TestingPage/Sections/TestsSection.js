8746afb42a0f1c63f8d63d24c8b77a98f7818f19
An Vo <30716416+tienlatien252@users.noreply.github.com>
2020-09-13 09:01:06 -0500

    Document the process of deploying GKE (#87)
    * Add backendhook
    
    * add use backend hook to the purcase tests page
    
    * add documentation on how to deloy to GKE
    
    * Fix answers wrap to each other and also display the previous answers
    
    * Change backend and frontend to depend on the enviroment variables
    
    * ignore .env files and use enviroment variables in the kubernetes cluster's secret
    
    * Document the process of connecting backend to database using Cloud SQL proxy
    
    * change small parts of the documentation

--- frontend/src/views/TestingPage/Sections/TestsSection.js
+++ frontend/src/views/TestingPage/Sections/TestsSection.js
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { auth } from "firebase.js";
-import axios from 'axios';
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-
-// @material-ui/icons
+import useDataApi from "../../../utils/BackendHook";
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 // core components
 import GridItem from "components/Grid/GridItem";
@@ -15,42 +13,31 @@ import CardBody from "components/Card/CardBody";
 
 import styles from "assets/jss/material-kit-react/views/landingPageSections/teamStyle";
 
-const useStyles = makeStyles(styles);
-
-const client = axios.create({
-    baseURL: 'http://localhost:5000',
-    json: true
-})
+const useStyles = makeStyles((theme) => ({
+    loading: {
+        width: '100%',
+        '& > * + *': {
+            marginTop: theme.spacing(2),
+        },
+    }, ...styles
+}));
 
 export default function TestsSection() {
     const classes = useStyles();
-
-    const [testArray, setTestArray] = useState([]);
+    const [{ data, isLoading, isError }, setRequest] = useDataApi();
 
     useEffect(() => {
-        async function fetchData() {
-            if (auth.currentUser) {
-                const idToken = await auth.currentUser.getIdToken(true);
-                try {
-                    const response = await client({
-                        method: 'get',
-                        url: '/auth/purchases',
-                        headers: {
-                            'AuthToken': idToken
-                        }
-                    });
-                    const tests = response.data["purchases"];
-                    setTestArray(tests);
-                } catch (error) {
-                    console.log(error);
-                }
-            }
+        setRequest({
+            method: 'get',
+            path: '/auth/purchases'
         }
-        fetchData();
+        );
     }, []);
 
     return (
-        testArray.map((test) =>
+        isLoading || !data.purchases ? <div className={classes.loading}>
+            <CircularProgress />
+        </div> : data.purchases.map((test) =>
             <GridItem xs={12} sm={12} md={4} key={test["test_id"]}>
                 <Card>
                     <CardHeader color="primary" className={classes.CardHeader}>
