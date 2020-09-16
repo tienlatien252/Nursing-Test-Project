ab76ba628c71d0a895de5116a1e40395660912f0
An Vo <thaiankh@gmail.com>
2020-09-15 19:13:23 -0500

    add userid to purchase table

--- backend/routes/stripe_webhook.js
+++ backend/routes/stripe_webhook.js
@@ -7,43 +7,61 @@ const { queryPostgres } = require('../postresql_client');
 const moment = require('moment');
 require('dotenv').config();
 
-router.post('/', bodyParser.raw({ type: "*/*" }), async function (request, response) {
-    let event;
-    let userID;
+async function addPurchaseInfoToDatabase(paymentIntent) {
     try {
-        if (request.body.metadata.userId) {
-            // event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-            event = request.body;
-            
-        } else {
+        const userId = paymentIntent.metadata.userId;
+        const purchase_time_stamp = moment(paymentIntent.created * 1000).format("YYYY-MM-DD HH:mm:ss");
+        const expire_time_stamp = moment(paymentIntent.created * 1000).add(12, 'month').format("YYYY-MM-DD HH:mm:ss");
+        const queryString = `INSERT INTO purchases(test_id , user_id, purchase_time, expire_time) VALUES ( 1, '${userId}' , '${purchase_time_stamp}','${expire_time_stamp}')`;
+
+        await queryPostgres(queryString);
+        console.log('PaymentIntent was successful!');
+    } catch (error) {
+        console.log(`Error when insert purchase to database : ${error.message}`);
+        throw Error(`Error when insert purchase to database : ${error.message}`);
+    }
+}
+
+async function testPaymentIntent(paymentSecret) {
+    const paymentIntent = await stripe.paymentIntents.retrieve(
+        paymentSecret
+    );
+    return paymentIntent;
+}
+
+router.post('/', bodyParser.raw({ type: "*/*" }), async function (request, response) {
+    const event = request.body;
+    //event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    try {
+        if (!event.data.object) {
+            return response.status('Event does not have correct data').end();
+        }
+
+        const paymentIntent = event.data.object;
+        // const paymentIntent = await stripe.paymentIntents.retrieve(
+        //     paymentSecret
+        // );
+        if (!paymentIntent.metadata || !paymentIntent.metadata.userId) {
             return response.status('Payment Intent does not have userID').end();
         }
+
+        switch (event.type) {
+            case 'payment_intent.succeeded':
+                await addPurchaseInfoToDatabase(paymentIntent);
+                break;
+            case 'payment_method.attached':
+                console.log('PaymentMethod was attached to a Customer!');
+                break;
+            default:
+                return response.status(400).status('Event type does not match').end();
+        }
+
+        response.json({ received: true });
     }
     catch (err) {
         console.log({ message: err.message, Error: err });
         return response.status('Webhook does not work').end();
     }
-    // Handle the event
-    switch (event.type) {
-        case 'payment_intent.succeeded':
-            const paymentIntent = event.data.object;//2323236
-            let purchase_time_stamp = moment(event.data.object.created * 1000).format("YYYY-MM-DD HH:mm:ss");
-            let expire_time_stamp = moment(event.data.object.created * 1000).add(12, 'month').format("YYYY-MM-DD HH:mm:ss");
-            const queryString = `INSERT INTO purchases(test_id ,purchase_time, expire_time) VALUES ( 1,'${purchase_time_stamp}','${expire_time_stamp}')`;
-            queryPostgres(queryString);
-            console.log('PaymentIntent was successful!');
-            break;
-        case 'payment_method.attached':
-            const paymentMethod = event.data.object;
-            console.log('PaymentMethod was attached to a Customer!');
-            break;
-        // ... handle other event types
-        default:
-            // Unexpected event type
-            return response.status(400).end();
-    }
-    // Return a 200 response to acknowledge receipt of the event
-    response.json({ received: true });
 })
 
 module.exports = router;
