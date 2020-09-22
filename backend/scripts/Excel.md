3d3ee8d0a54f859d8a6659fbb00a65d0f02f8e01
Cong Nguyen <howardnguyen69@gmail.com>
2020-09-22 18:11:06 -0500

    adding complete question

---
+++ backend/scripts/Excel.md
@@ -0,0 +1,5 @@
+EXCEL commands:
+Check whether or not between the ANSWERS and CORRECT_ANSWER columns: 
+ =IF(SEARCH(CORRECT_ANSWER,ANSWERS),TRUE,FALSE)
+
+Removing trailing and leading spaces in Excel: Use TRIM() Function -> https://www.ablebits.com/office-addins-blog/2013/11/15/trim-spaces-between-words/
---
+++ backend/scripts/adding_complete_questions_to_database.sql
@@ -0,0 +1,24 @@
+Python Script to Insert data to databases:
+    file populate_db.py in scripts
+
+CREATE TABLES IN POSTRESQL: 
+    CREATE TABLE purchases (
+	test_id BIGINT REFERENCES tests(test_id),
+	expire_time TIMESTAMP,
+    purchase_time TIMESTAMP,
+    user_id TEXT
+);
+ CREATE TABLE questions (
+     test_name TEXT,
+     test_description TEXT,
+     test_price BIGINT,
+     test_id BIGINT REFERENCES tests(test_id)
+);
+CREATE TABLE tests (
+	test_id BIGINT PRIMARY KEY,
+    test_name TEXT,
+    test_description TEXT,
+    test_price BIGINT
+);
+
+ 
